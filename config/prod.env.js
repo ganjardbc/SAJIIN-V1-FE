59bcd10d86073c7b6ac8b2a12c2caff0d4d09dbe
Ganjar Hadiatna <mdl.ganjarhadiatna@gmail.com>
2025-01-01 10:39:59 +0700

    update: api pointing

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_URL: '"https://api.sajiin.id"',
+  BASE_URL: '"https://apps.sajiin.id"',
   API_URL: '"https://api.sajiin.id"',
   SOCKET_URL: '"https://websocket.sajiin.id"',
   PRINTER_URL: '"http://localhost:9999"',
