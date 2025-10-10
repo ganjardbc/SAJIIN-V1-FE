954d116dc4a00189f523d74bfb60b512e5b8d4fb
Ganjar Hadiatna <mdl.ganjarhadiatna@gmail.com>
2025-10-11 00:02:38 +0700

    change port to 3000

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,7 +4,7 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"http://localhost:8080/"',
+  BASE_URL: '"http://localhost:3000/"',
   API_URL: '"http://localhost:8000"',
   SOCKET_URL: '"https://websocket.sajiin.id/"',
   PRINTER_URL: '"http://localhost:9999"',
